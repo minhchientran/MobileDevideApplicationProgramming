8851f885c31a6134b3a817c418ba2bca3381f750
minhchientran <minhchien.arashino@gmail.com>
2020-04-18 15:11:50 +0700

    Solved Hamming

--- Week1/hamming/src/main/kotlin/Hamming.kt
+++ Week1/hamming/src/main/kotlin/Hamming.kt
@@ -1,6 +1,16 @@
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        TODO("Implement this function to complete the task")
+        //TODO("Implement this function to complete the task")
+        if (leftStrand.length > rightStrand.length || leftStrand.length < rightStrand.length) {
+            throw IllegalArgumentException("left and right strands must be of equal length")
+        }
+        var difference = 0
+        for (index in leftStrand.indices) {
+            if (leftStrand[index] != rightStrand[index]) {
+                difference++
+            }
+        }
+        return difference
     }
 }
