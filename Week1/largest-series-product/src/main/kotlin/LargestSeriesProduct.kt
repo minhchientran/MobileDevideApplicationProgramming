42f943e9cef8cc418808cb674ca1d70c23dd3ff1
minhchientran <minhchien.arashino@gmail.com>
2020-04-19 20:39:59 +0700

    Solve Largest Series Product

--- Week1/largest-series-product/src/main/kotlin/LargestSeriesProduct.kt
+++ Week1/largest-series-product/src/main/kotlin/LargestSeriesProduct.kt
@@ -1,8 +1,31 @@
-class Series {
+class Series (input: String){
 
     // TODO: Implement proper constructor
-
+    var series = input
+    init {
+        var digit = "^[0-9]*$".toRegex()
+        if(!digit.matches(series)){
+            throw IllegalArgumentException()
+        }
+    }
     fun getLargestProduct(span: Int): Long {
-        TODO("Implement this function to complete the task")
+        //TODO("Implement this function to complete the task")
+        if (span > 0 && series.isEmpty()) {
+            throw IllegalArgumentException()
+        }
+        if (span > series.length || span < 0) {
+            throw IllegalArgumentException()
+        }
+        var largestProduct : Long = 0
+        for (i in 0 .. (series.length - span)) {
+            var tempProduct : Long = 1
+            for (j in 0 until span) {
+                tempProduct *= series[i+j].toString().toInt()
+            }
+            if (tempProduct > largestProduct) {
+                largestProduct = tempProduct
+            }
+        }
+        return largestProduct
     }
 }
