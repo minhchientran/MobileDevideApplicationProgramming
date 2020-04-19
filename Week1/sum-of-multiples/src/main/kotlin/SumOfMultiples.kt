711fb15418b550ee6766d926da82316dd884a06d
minhchientran <minhchien.arashino@gmail.com>
2020-04-19 22:42:14 +0700

    Solve Sum of Multiples

--- Week1/sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
+++ Week1/sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
@@ -1,6 +1,20 @@
 object SumOfMultiples {
 
     fun sum(factors: Set<Int>, limit: Int): Int {
-        TODO("Implement this function to complete the task")
+        //TODO("Implement this function to complete the task")
+
+        var result = 0
+        for (i in 1 until limit) {
+            for (j in factors) {
+                if (j == 0) {
+                    break
+                }
+                if (i.rem(j) == 0) {
+                    result += i
+                    break
+                }
+            }
+        }
+        return result
     }
 }
